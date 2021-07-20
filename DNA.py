1474159e5db44f1674a918a1a209d7c7e5550f13
LizaTs <86945966+LizaTs@users.noreply.github.com>
2021-07-20 14:11:46 +0300

    Add files via upload

--- DNA.py
+++ DNA.py
@@ -1,74 +1,70 @@
-#read files
 import csv
 
-def data(csvfile): #opens the information stored in database as a list of dictionaries
+def data(csvfile): 
+# Opens the information stored in database as a list of dictionaries
 	path = 'C:/Users/Soffi/Desktop/liza/dna/databases/' + csvfile
 	with open(path) as file:
-		raw_database = csv.DictReader(file)
-		database = list(raw_database)
-		return database
+		database = csv.DictReader(file)
+		return list(database)
 
-def sequence(file): #opens files to be matched to database
-	path = 'C:/Users/Soffi/Desktop/liza/dna/sequences/' + file
+def sequence(file):
+# Opens files to be matched to database
+	path ='C:/Users/Soffi/Desktop/liza/dna/sequences/' +  file
 	with open(path) as raw_seq:
 		seq = raw_seq.read()
 	return seq
 
-###################################################################################################################
+###############################################################################################################
 if __name__ == '__main__':
-	#empy (for now) variables
-	seqs = {}
-	people = [] #contains dictionaries of info about people inside the database
-	per_vals = [] #same as people, in the form of list
-	names = [] #kinda unzips per_vals. creates list of names of people inside the database
-	vals = [] #creates list of the count of dna sequences of people inside the database
+	# Empy (for now) variables
+	seqs_count = {}
+	people = [] # Contains dictionaries of info about people inside the database
+	per_vals = [] # Same as people, in the form of list
+	names = [] # Kinda unzips per_vals. creates list of names of people inside the database
+	vals = [] # Creates list of the count of dna sequences of people inside the database
 
-	#read files
-	database = data(input('plaese enter database: '))#calls "data" function and creates small database
-	seq = sequence(input('please enter file name: ')) #calls "sequence" function and makes data usable
+	# Read files
+	database = data(input('plaese enter database: '))# Calls "data" function and creates small database
+	seq = sequence(input('please enter file name: ')) # Calls "sequence" function and makes data usable
 
-	#count sequences
+	# Count sequences
 	dna = dict(database[1]).keys()
 	dna = list(dna)
 	dna = dna[1:]
 
 	for dat in dna:
 		index = 0
-		real_count = 0
-		temp_count = 0
+		r_count = 0 # The real, final count of sequences
+		t_count = 0 # A temporary count of sequences. Becomes r_count when sequence is longest
 		while index < len(seq):
 			if seq[index:index + len(dat)] == dat:
-				temp_count = temp_count + 1
+				t_count = t_count + 1
 				index = index + len(dat)
 			else:
-				if temp_count > real_count:
-					real_count = temp_count
-				temp_count = 0
+				if t_count > r_count:
+					r_count = t_count
+				t_count = 0
 				index = index + 1
-		seqs[dat] = str(real_count)
-	#print(seqs[dat])
-	suspect = list(seqs.values()) #contains info of each "suspect"
-#match sequences
+		seqs_count[dat] = str(r_count)
 
-for line in database: #creates dictionaries with info of each person inside the database
-	person = dict(line)
-	people.append(person)
+	##########################################################################################################
+	# Match sequences
 
-for person in people: # converts data inside dictinaries into lists
-	person = person.values()
-	per_vals.append(list(person))
-#__________________________________________
+	suspect = list(seqs_count.values()) # Contains info of each "suspect"
 
-#match lists
+	for person in database: # Creates dictionaries with info of each person inside the database
+		people.append(person.values())
 
-for person in per_vals:
-	names.append(person[0])
-	vals.append(person[1:])
+	#match lists
+	for person in people:
+		person = list(person)
+		names.append(person[0])
+		vals.append(person[1:])
 
-for person in vals:
-	if suspect in vals:
-		criminal = vals.index(suspect)
-		print(names[criminal])
-	else:
-		print("no match")
-	break
+	for person in vals:
+		if suspect in vals:
+			criminal = vals.index(suspect)
+			print(names[criminal])
+		else:
+			print("no match")
+		break
